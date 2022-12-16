24f0ee5d14429be7a2eeadf58744f289eaf41598
JezzeX <jezzejagz4@gmail.com>
2022-12-16 22:01:50 +0100

    Done

--- script.js
+++ script.js
@@ -1,77 +1,53 @@
-"use strict";
+const compChoice = ["ROCK", "PAPER", "SCISSORS"];
 
-//Variables
-const choice = ["ROCK", "PAPER", "SCISSORS"];
-var computerSelection;
-var playerSelection;
-var playerScore = 0;
-var compScore = 0;
+const button = document.querySelectorAll("button");
 
-//Functions
+const div = document.querySelector("div");
+
+const p = document.querySelector("p");
+
 function getComputerChoice() {
-  return choice[Math.floor(Math.random() * 3)];
+  return compChoice[Math.floor(Math.random() * 3)];
 }
 
-function getPlayerChoice() {
-  let ask = prompt("Enter Rock, Paper or Scissors");
-  return ask.toUpperCase();
-}
+button.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    playRound(e.target.id, getComputerChoice());
+  });
+});
 
 function playRound(player, comp) {
+  p.textContent = `Computer chose ${comp}`;
   switch (true) {
     case player === comp:
-      console.log("It's a draw");
+      div.textContent = "It's a draw";
       break;
 
     case player === "ROCK" && comp === "SCISSORS":
-      console.log("Player wins");
-      return playerScore++;
+      div.textContent = "Player wins";
       break;
 
     case player === "PAPER" && comp === "ROCK":
-      console.log("Player wins");
-      return playerScore++;
+      div.textContent = "Player wins";
       break;
 
     case player === "SCISSORS" && comp === "PAPER":
-      console.log("Player wins");
-      return playerScore++;
+      div.textContent = "Player wins";
       break;
 
     case comp === "ROCK" && player === "SCISSORS":
-      console.log("Comp Wins");
-      return compScore++;
+      div.textContent = "Comp wins";
       break;
 
     case comp === "PAPER" && player === "ROCK":
-      console.log("Comp wins");
-      return compScore++;
+      div.textContent = "Comp wins";
       break;
 
     case comp === "SCISSORS" && player === "PAPER":
-      console.log("Player wins");
-      return compScore++;
+      div.textContent = "Comp wins";
       break;
 
     default:
       console.log("Enter Rock, Paper or Scissors correctly");
   }
 }
-
-function game() {
-  for (let i = 1; i <= 5; i++) {
-    playerSelection = getPlayerChoice();
-    computerSelection = getComputerChoice();
-    playRound(playerSelection, computerSelection);
-  }
-
-  if (playerScore > compScore) {
-    console.log("Player wins game");
-  } else if (compScore > playerScore) {
-    console.log("Comp wins game");
-  } else {
-    console.log("It's a draw");
-  }
-}
-
-game();
